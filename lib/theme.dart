7a350744cb14f3cf5766948c876841d604465b67
Prima Interaktif - Developer <ahmdfatihin17@gmail.com>
2022-10-13 13:11:59 +0700

    feat: add theme data

---
+++ lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// Color Pallete
+Color kBlackColor = const Color(0xff020E27).withOpacity(0.7);
+Color kWhiteColor = const Color(0xFFFFFFFF).withOpacity(0.7);
+
+ThemeData lightThemeData(BuildContext context) {
+  return ThemeData.light().copyWith(
+      primaryColor: Colors.white.withOpacity(0.2),
+      scaffoldBackgroundColor: kWhiteColor);
+}
+
+ThemeData darkThemeData(BuildContext context) {
+  // Bydefault flutter provie us light and dark theme
+  // we just modify it as our need
+  return ThemeData.dark().copyWith(
+      primaryColor: Colors.black.withOpacity(0.2),
+      scaffoldBackgroundColor: kBlackColor);
+}

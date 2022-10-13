927aa531b971caed7b33462228d91d7a36510c20
Prima Interaktif - Developer <ahmdfatihin17@gmail.com>
2022-10-13 16:05:49 +0700

    refactor: refactor unused statefull widget cause we have state management dude

--- lib/module/calculator/section/display.dart
+++ lib/module/calculator/section/display.dart
@@ -2,22 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:glassmorphism_ui/glassmorphism_ui.dart';
+import 'package:my_calculator/module/calculator/component/header.dart';
 import 'package:my_calculator/module/calculator/state/cubit/theme_cubit.dart';
 import 'package:my_calculator/theme.dart';
 
-class Display extends StatefulWidget {
+class Display extends StatelessWidget {
   final String text;
   Display(this.text);
 
-  @override
-  State<Display> createState() => _DisplayState();
-}
-
-bool _iconBool = false;
-IconData _iconLight = Icons.wb_sunny;
-IconData _iconDark = Icons.nights_stay;
-
-class _DisplayState extends State<Display> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -27,10 +19,11 @@ class _DisplayState extends State<Display> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            Header(),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: AutoSizeText(
-                widget.text,
+                text,
                 minFontSize: 20,
                 maxFontSize: 80,
                 maxLines: 1,
